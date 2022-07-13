9f380a6832e767790b3db3c9451cca46309483c1
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2022-07-13 13:35:25 +0200

    Added aws_backup_plan resource

--- main.tf
+++ main.tf
@@ -5,3 +5,41 @@ resource "aws_backup_vault" "backup_vault" {
   kms_key_arn = var.backup_vault_kms_key_arn
   tags        = var.backup_vault_tags
 }
+
+resource "aws_backup_plan" "backup_plan" {
+  count = var.create_backup_plan ? 1 : 0
+
+  name = var.backup_plan_name
+
+  dynamic "rule" {
+    for_each = var.backup_plan_rule != null ? [var.backup_plan_rule] : []
+
+    content {
+      rule_name                = rule.value["rule_name"]
+      target_vault_name        = aws_backup_vault.backup_vault[0].name
+      schedule                 = try(rule.value["schedule"], null)
+      enable_continuous_backup = try(rule.value["enable_continuous_backup"], null)
+      start_window             = try(rule.value["start_window"], null)
+      completion_window        = try(rule.value["completion_window"], null)
+      recovery_point_tags      = try(rule.value["recovery_point_tags"], null)
+
+      dynamic "lifecycle" {
+        for_each = try([rule.value["lifecycle"]], [])
+
+        content {
+          cold_storage_after = lookup(lifecycle.value, "cold_storage_after", null)
+          delete_after       = lookup(lifecycle.value, "delete_after", null)
+        }
+      }
+    }
+  }
+
+  dynamic "advanced_backup_setting" {
+    for_each = var.backup_plan_advanced_backup_setting != null ? [var.backup_plan_advanced_backup_setting] : []
+
+    content {
+      backup_options = each.value.backup_options
+      resource_type  = each.value.resource_type
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -8,6 +8,7 @@ variable "create_backup_vault" {
 }
 
 variable "backup_vault_name" {
+  default     = null
   description = "Required if `create_backup_vault` is set to `true`. Name of the backup vault to create"
   type        = string
 }
@@ -22,4 +23,45 @@ variable "backup_vault_tags" {
   default     = null
   description = "Metadata that you can assign to help organize the resources that you create"
   type        = map(string)
+}
+
+####################################################################################################
+### aws_backup_plan
+####################################################################################################
+variable "create_backup_plan" {
+  default     = true
+  description = "Determinate to create `aws_backup_vault` resources or not. "
+  type        = bool
+}
+
+variable "backup_plan_name" {
+  default     = null
+  description = "Required if `create_backup_plan` is set to `true`. The display name of a backup plan"
+  type        = string
+}
+
+variable "backup_plan_rule" {
+  default     = null
+  description = "Reuiqred if `create_backup_plan` is set to `true`. A rule object that specifies a scheduled task that is used to back up a selection of resources"
+  type = object({
+    rule_name                = string
+    schedule                 = optional(string)
+    enable_continuous_backup = optional(bool)
+    start_window             = optional(number)
+    completion_window        = optional(number)
+    recovery_point_tags      = optional(map(string))
+    lifecycle = optional(object({
+      cold_storage_after = optional(number)
+      delete_after       = optional(number)
+    }))
+  })
+}
+
+variable "backup_plan_advanced_backup_setting" {
+  default     = null
+  description = "An object that specifies backup options for each resource type"
+  type = object({
+    backup_options = map(string)
+    resource_type  = string
+  })
 }
